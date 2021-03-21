e7c685205d0489e440a55aac904243029667e2f9
doyeon <jdy0210@naver.com>
2021-03-21 13:26:56 +0900

    fetch example

---
+++ doyeon.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var user = {
+    name: 'doyeon',
+    age: '28',
+    address: '전라북도 익산시 목천로 346'
+};
---
+++ doyeon.tsx
@@ -0,0 +1,11 @@
+export type MyType = {
+  name: string;
+  age: string;
+  address: string;
+}
+
+const user:MyType = {
+  name:'doyeon',
+  age: '28',
+  address: '전라북도 익산시 목천로 346'
+}
